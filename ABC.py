a8c44d02fade4ce2ed2463b03d3868b2137a58e3
Vikashdwivedi123 <88416590+Vikashdwivedi123@users.noreply.github.com>
2021-08-04 09:34:33 +0530

    Create ABC.py
    I write a program,to print an area of circle when user get an input value radius is 1.1

---
+++ ABC.py
@@ -0,0 +1,11 @@
+- from math import pi
+#write input of the radius is 1.1
+r=float(input("input the radius of circle is:"))
+area=pi*r**2
+Print("area of circle with radius is:",area)
+
+
+<!---
+Vikashdwivedi123/Vikashdwivedi123 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->

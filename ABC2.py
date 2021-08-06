99b9ab8d1deca10fd0cbe214e6c81b7f7a388841
Vikashdwivedi123 <88416590+Vikashdwivedi123@users.noreply.github.com>
2021-08-06 22:27:00 +0530

    Create ABC2.py
    We write a program of second task which is given by you sir .

---
+++ ABC2.py
@@ -0,0 +1,16 @@
+List1=[12,-7,5,-14,64]
+for num in list1:
+   if num>=0:
+      Print(num,end=' ')
+
+
+#we write a program of second question!!
+
+List2=[12,14,-95,3]
+List2.remove(-95)
+Print(List2)
+
+<!---
+Vikashdwivedi123/Vikashdwivedi123 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->

bfa2c86de7ce44474f33e646f4db869bf1eb5ddd
Vikashdwivedi123 <88416590+Vikashdwivedi123@users.noreply.github.com>
2021-08-15 19:21:50 +0530

    Create Abc4.py
    Program of Mississippi

---
+++ Abc4.py
@@ -0,0 +1,13 @@
+W=input("please enter a string")
+def most_frequent(string):
+d=dict()
+for key in string:
+if key not in d:
+d[key] += 1
+return d
+Print(most_frequent(W))
+
+<!---
+Vikashdwivedi123/Vikashdwivedi123 is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
